6f21b28ed5d460b262bbd0e02b4113312d29775c
Martin Poulsen <mapo0004@stud.kea.dk>
2025-01-05 15:13:36 +0100

    add

---
+++ src/main/java/exam/kea/dk/deletelater/GreetingController.java
@@ -0,0 +1,24 @@
+package exam.kea.dk.deletelater;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private final GreetingRepository greetingRepository;
+
+    public GreetingController(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Greeting> greeting() {
+        Greeting greeting = new Greeting("Hej fra serveren");
+        greetingRepository.save(greeting);
+        return new ResponseEntity<>(greeting, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/exam/kea/dk/deletelater/GreetingRepository.java
@@ -0,0 +1,6 @@
+package exam.kea.dk.deletelater;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface GreetingRepository extends CrudRepository<Greeting, Long> {
+}
